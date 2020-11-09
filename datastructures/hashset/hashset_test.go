27d7f58ba054ec281013cf03a83026d7e688466a
inno <893930581@qq.com>
2020-11-09 16:14:29 +0800

    feat(hashset): do test

--- datastructures/hashset/hashset_test.go
+++ datastructures/hashset/hashset_test.go
@@ -1,1 +1,20 @@
+package hashset
 
+import "testing"
+
+func TestAdd(t *testing.T) {
+	s := New("string")
+	s.Add("foo")
+	if ok, _ := s.Contain("foo"); !ok {
+		t.Errorf("wanted has: %v, but nil", "foo")
+	}
+}
+
+func TestClear(t *testing.T) {
+	s := New("string")
+	s.Add("foo")
+	s.Clear()
+	if ok, _ := s.Contain("foo"); ok {
+		t.Errorf("want nil, get: %v", "foo")
+	}
+}
